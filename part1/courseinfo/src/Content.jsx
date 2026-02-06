46a20015ac8cea9f384bdb2caf183ba611b6d241
Tiberiw <amarietiberiu@yahoo.com>
2026-02-06 16:02:32 +0200

    Ex 1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,10 +1,11 @@
+import Part from './Part'
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
     return (
-        <>
-            <p>{part1} {exercises1}</p>
-            <p>{part2} {exercises2}</p>
-            <p>{part3} {exercises3}</p>
-        </>
+        <div>
+            <Part title={part1} noExercises={exercises1} />
+            <Part title={part2} noExercises={exercises2} />
+            <Part title={part3} noExercises={exercises3} />
+        </div>
     )
 };
 
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,5 @@
+const Part = ({title, noExercises}) => {
+    return <p>{title} {noExercises}</p>
+};
+
+export default Part;
