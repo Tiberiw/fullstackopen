0f3d02d91f5e99270c8860f5e7f57c997c5de60d
Tiberiw <amarietiberiu@yahoo.com>
2026-02-06 15:49:41 +0200

    Ex 1.1

---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,2 @@
+const Header = ({course}) => <h1>{course}</h1>; // the component takes an OBJECT as params. That object is props and has course poperty
+export default Header;
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,10 @@
+const Total = (props) => {
+    const exercises1 = props.exercises1;
+    const exercises2 = props.exercises2;
+    const exercises3 = props.exercises3;
+    return (
+        <p>Number of exercises is {exercises1 + exercises2 + exercises3}</p>
+    )
+};
+
+export default Total;
