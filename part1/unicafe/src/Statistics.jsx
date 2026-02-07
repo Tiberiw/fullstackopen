95f326e7c39f55e72fea6241e7bac14cb2e923f4
Tiberiw <amarietiberiu@yahoo.com>
2026-02-07 15:51:45 +0200

    Ex 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,7 +1,15 @@
 const Statistics = ({good, neutral, bad}) => {
-        const total = good + bad + neutral;
-        const average = (good - bad) / total;
-        const positive_percentage = 100 * good / total;
+    const total = good + bad + neutral;
+    if (total === 0) {
+        return (
+            <>
+                <h2>statistics</h2>
+                <p> No feedback given </p>
+            </>
+        )
+    }
+    const average = (good - bad) / total;
+    const positivePercentage = 100 * good / total;
     return (
         <>
             <h2>statistics</h2>
@@ -10,7 +18,7 @@ const Statistics = ({good, neutral, bad}) => {
             <p>bad {bad}</p>
             <p>all {total}</p>
             <p>average {average}</p>
-            <p>positive {positive_percentage} %</p>
+            <p>positive {positivePercentage} %</p>
         </>
     )
 }
