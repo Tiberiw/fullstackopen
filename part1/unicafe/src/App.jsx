92575fc3eb510d7507420247d69fc2e61497f5ea
Tiberiw <amarietiberiu@yahoo.com>
2026-02-07 11:24:33 +0200

    Ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
+  const positive_percentage = 100 * good / total;
   return (
     <>
       <h1>give feedback</h1>
@@ -14,6 +17,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive_percentage} %</p>
     </>
   )
 }
