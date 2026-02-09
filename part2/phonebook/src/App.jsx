3cd7a893582311f54e440ddf1f418b6c343841a5
Tiberiw <amarietiberiu@yahoo.com>
2026-02-09 08:49:02 +0200

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('');
   const [nameFilter, setNameFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+  }, [])
+
   const contactsForDisplay = nameFilter === '' 
   ? persons
   : persons.filter(it => it.name.includes(nameFilter))
@@ -39,7 +42,7 @@ function App() {
 
     const newPerson = {
       name: newName,
-      phone: newPhone,
+      number: newPhone,
       id: crypto.randomUUID()
     };
     setPersons(prev => prev.concat(newPerson))
