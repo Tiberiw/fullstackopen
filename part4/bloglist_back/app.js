909028b9538537697f3d62c7af233aee8737cc25
Tiberiw <amarietiberiu@yahoo.com>
2026-03-12 22:11:53 +0200

    Ex 4.2

---
+++ part4/bloglist_back/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require("./utils/config");
+const logger = require("./utils/logger")
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4'])
+
+const app = express()
+
+logger.info('Connecting to', config.MONGODB_URI)
+mongoose
+    .connect(config.MONGODB_URI, { family: 4 })
+    .then(() => logger.info("Connected to MongoDB"))
+    .catch(err => logger.error("Error connecting to MongoDB", err.message))
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/bloglist_back/controllers/blogs.js
@@ -0,0 +1,14 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => response.json(blogs))
+})
+
+blogRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+    blog.save()
+        .then(blog => response.status(201).json(blog))
+})
+
+module.exports = blogRouter
